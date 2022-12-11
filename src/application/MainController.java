f6a46032c0765c9a2211764d3fad10eb19202092
Naga Venkatesh Gavini <nagavenkat9948@gmail.com>
2022-12-10 23:30:03 -0500

    Fixes and updations

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -6,11 +6,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.ScatterChart;
@@ -52,6 +61,11 @@ public class MainController {
     private Button btnPieChart;
     
     @FXML
+    private LineChart<String, Number> lineChart;
+    
+    XYChart.Series lineChartData = new XYChart.Series();
+    
+    @FXML
     private TextField txt1Field;
     
     @FXML
@@ -62,12 +76,20 @@ public class MainController {
     
     @FXML
     private NumberAxis yAxis;
+    
+    @FXML
+    private CategoryAxis xAxisLine;
+
+    @FXML
+    private NumberAxis yAxisLine;
         
     ObservableList<String> featureList = FXCollections.observableArrayList();
     
     private String[] headers;
     
     XYChart.Series barChartData = new XYChart.Series();
+    XYChart.Series scatterChartData = new XYChart.Series();
+
     
     ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
     
@@ -92,13 +114,21 @@ public class MainController {
     	
     	FileChooser fc = new FileChooser();
     	
-    	fc.getExtensionFilters().addAll(new ExtensionFilter("CSV Files", "*.csv"));
+    	String path = null;
+		String[] headers = null;
+		
+		fc.getExtensionFilters().addAll(new ExtensionFilter("CSV Files", "*.csv"));	
+		File selectedFile = fc.showOpenDialog(null);
+		
+		
+		try {
+			path = selectedFile.getAbsolutePath();
+			headers = readHeader(path);
+		} catch (IOException e) {
+			System.out.println("File not read properly, please select file from the dialog box");
+		}
     	
-    	File selectedFile = fc.showOpenDialog(null);
-    	
-    	String path = selectedFile.getAbsolutePath();
-    	String[] headers = readHeader(path);
-    	
+    	// Use sets here to eliminate the headers data
 		for (int i = 0; i < headers.length; i++) {
 			listview.getItems().add(headers[i]);
 			featureList.add(headers[i]);
@@ -108,7 +138,6 @@ public class MainController {
     	featureBox2.setItems(featureList);
     	
     	txt1Field.setText(path);
-    	
     }
     
     
@@ -123,6 +152,7 @@ public class MainController {
  		categoryList.clear();
  		countList.clear();
  		
+ 		pieChart1.setAnimated(false);	 		
  		pieChart1.getData().removeAll(pieChartData);
  		pieChartData.removeAll(pieChartData);
  		
@@ -135,7 +165,6 @@ public class MainController {
 			
 									
 			//reading all the values
-			
 			for (int i = 0; i < headers.length; i++) {
 				if (featureBox.getValue().equals(headers[i])) {
 					while((line = br.readLine()) != null) {
@@ -154,8 +183,7 @@ public class MainController {
 				for (int b = 0; b < categoryList.size(); b++) {
 										
 					if (!rowList.get(m).equals(categoryList.get(b))) {
-						times++;
-							
+						times++;	
 					}
 					
 				}
@@ -180,7 +208,6 @@ public class MainController {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -207,12 +234,15 @@ public class MainController {
 		
  		ArrayList<String> rowList = new ArrayList<>();
  		ArrayList<String> categoryList = new ArrayList<>();
+ 		ArrayList<Double> numericList = new ArrayList<>();
  		ArrayList<Integer> countList = new ArrayList<>();
  		
  		rowList.clear();
  		categoryList.clear();
+ 		numericList.clear();
  		countList.clear();
  		
+ 		barChart1.setAnimated(false);	 		
  		barChartData.getData().clear();
  		barChart1.getData().removeAll(barChartData);
  		
@@ -236,35 +266,96 @@ public class MainController {
 			}
 			
 			
-			categoryList.add(rowList.get(0));
-			
-			for (int m = 1; m < rowList.size(); m++) {
+			if (rowList.get(0).charAt(0)== '-' || rowList.get(0).charAt(0)== '0' || rowList.get(0).charAt(0)== '1' || rowList.get(0).charAt(0)== '2'
+					 || rowList.get(0).charAt(0)== '3' || rowList.get(0).charAt(0)== '4' || rowList.get(0).charAt(0)== '5' || rowList.get(0).charAt(0)== '6'
+					 || rowList.get(0).charAt(0)== '7' || rowList.get(0).charAt(0)== '8' || rowList.get(0).charAt(0)== '9') {
 				
-				int times = 0;
-				for (int b = 0; b < categoryList.size(); b++) {
-										
-					if (!rowList.get(m).equals(categoryList.get(b))) {
-						times++;
-							
-					}
-					
-				}
-				
-				if (times == categoryList.size()) {
-					categoryList.add(rowList.get(m));
-					
-				}
+				numericList.add(Double.valueOf(rowList.get(0)));
+ 				
+ 				for (int m = 1; m < rowList.size(); m++) {
+ 					
+ 					int times = 0;
+ 					for (int b = 0; b < numericList.size(); b++) {
+ 						
+ 						double s = Double.valueOf(rowList.get(m));
+ 						double v = numericList.get(b); 
+ 						
+ 						
+ 						if (s != v) {
+ 							
+ 							times++;
+ 								
+ 						}
+ 						
+ 					}
+ 					 	 					
+ 					if (times == numericList.size()) {
+ 						numericList.add(Double.valueOf(rowList.get(m)));
+ 						
+ 					}
+ 				}
+ 				
+ 				Collections.sort(numericList);
+ 				
+ 				
+ 				for (int s = 0; s < numericList.size(); s++) {
+ 					
+ 					int count = 0;
+ 					for (int n = 0; n < rowList.size(); n++) {
+ 						
+ 						double z = Double.valueOf(rowList.get(n));
+ 						
+ 						double w = numericList.get(s);
+ 						;
+ 						 	 						
+ 						if (z == w) {
+ 							
+ 							count++;
+ 							
+ 							}
+ 						
+ 					}
+ 					
+ 					
+ 					countList.add(count);
+ 				
+ 					
+ 					
+ 				}
+			}else {
+				categoryList.add(rowList.get(0));
+ 				
+ 				for (int m = 1; m < rowList.size(); m++) {
+ 					
+ 					int times = 0;
+ 					for (int b = 0; b < categoryList.size(); b++) {
+ 											
+ 						if (!rowList.get(m).equals(categoryList.get(b))) {
+ 							times++;
+ 								
+ 						}
+ 						
+ 					}
+ 					
+ 					if (times == categoryList.size()) {
+ 						categoryList.add(rowList.get(m));
+ 						
+ 					}
+ 				}
+ 				
+ 				for (int s = 0; s < categoryList.size(); s++) {
+ 					int count = 0;
+ 					for (int n = 0; n < rowList.size(); n++) {
+ 						if (rowList.get(n).equals(categoryList.get(s))) {
+ 							count++;
+ 							}
+ 					}
+ 					countList.add(count);
+ 				}
+ 				
+ 				
 			}
 			
-			for (int s = 0; s < categoryList.size(); s++) {
-				int count = 0;
-				for (int n = 0; n < rowList.size(); n++) {
-					if (rowList.get(n).equals(categoryList.get(s))) {
-						count++;
-						}
-				}
-				countList.add(count);
-			}
 			
 	
 		} catch (FileNotFoundException e) {
@@ -274,49 +365,72 @@ public class MainController {
 			e.printStackTrace();
 		}
 		
-		
-		
-		
-       for (int i = 0; i < categoryList.size(); i++) {
-    	   barChartData.getData().add(new XYChart.Data(categoryList.get(i), countList.get(i)));
-       }
-       
-              
-       
-       if (barChart1 != null) {
-    	   
-    	   barChart1.getData().addAll(barChartData);
-    	   barChart1.setTitle(featureBox.getValue());
-       }else {
-    	   System.out.println("It is null!!!!!");
-       }
-
-       
+		if (numericList.size() > categoryList.size()) {
+				for (int i = 0; i < numericList.size(); i++) {
+					barChartData.getData().addAll(new XYChart.Data(String.valueOf(numericList.get(i)), countList.get(i)));
+ 	 	       }
+ 	 	       
+ 	 	              
+ 	 	       
+ 	 	       if (barChart1 != null) { 
+ 	 	    	   barChart1.getData().add(barChartData);
+ 	 	    	   barChart1.setTitle(featureBox.getValue());
+ 	 	       }else {
+ 	 	    	   System.out.println("It is null!!!!!");
+ 	 	       }
+			}else {
+				for (int i = 0; i < categoryList.size(); i++) {
+			    	   barChartData.getData().add(new XYChart.Data(categoryList.get(i), countList.get(i)));
+			       }
+			       
+			              
+			       
+			       if (barChart1 != null) {
+			    	   
+			    	   barChart1.getData().addAll(barChartData);
+			    	   barChart1.setTitle(featureBox.getValue());
+			       }else {
+			    	   System.out.println("It is null!!!!!");
+			       }
+			}
 	}
  	
  	public void showScatterPlot() throws IOException {
  		
-        XYChart.Series series1 = new XYChart.Series();
-
         scatterChart.setAnimated(false);
-        scatterChart.getData().clear();
+ 		scatterChartData.getData().clear();
+ 		scatterChart.getData().removeAll(scatterChartData);
+ 		scatterChart.setTitle("");
+        
+        ArrayList<String> colXData = new ArrayList<>();
+        ArrayList<String> colYData = new ArrayList<>();
+		ArrayList<String> newColXData = new ArrayList<String>();
+		ArrayList<Double> tempList = new ArrayList<Double>();
 
+        colXData.clear();
+        colYData.clear();
+        newColXData.clear();
+        tempList.clear();
+        
 		String dropDownXValString = featureBox.getValue();
 		String dropDownYValString = featureBox2.getValue();
 				
 	    xAxis.setLabel(dropDownXValString);  
 	    yAxis.setLabel(dropDownYValString);
 	    
-	    BufferedReader br = new BufferedReader(new FileReader(txt1Field.getText()));
+	    // file objs to read it multiple timesfor colx and coly
+	    HashMap<String, BufferedReader> fileObjs = new HashMap<String, BufferedReader>();
+	    fileObjs.clear();
+	    fileObjs.put("xAxisReader", new BufferedReader(new FileReader(txt1Field.getText())));
+	    fileObjs.put("yAxisReader", new BufferedReader(new FileReader(txt1Field.getText())));
 		
-		String line = br.readLine();
+		String line = fileObjs.get("xAxisReader").readLine();
 		String[] headers = line.split(",");
 		
 		// Read x col data
-		ArrayList<String> colXData = new ArrayList<>();
 		for (int i = 0; i < headers.length; i++) {
 			if (dropDownXValString.equals(headers[i])) {
-				while((line = br.readLine()) != null) {
+				while((line = fileObjs.get("xAxisReader").readLine()) != null) {
 					String[] values = line.split(",");
 					colXData.add(values[i]);
 				}
@@ -324,29 +438,185 @@ public class MainController {
 		}
 
 		// Read Y col data
-		BufferedReader br2 = new BufferedReader(new FileReader(txt1Field.getText()));
-		ArrayList<String> colYData = new ArrayList<>();
+		String dummyRead = fileObjs.get("yAxisReader").readLine();
 		for (int i = 0; i < headers.length; i++) {
 			if (dropDownYValString.equals(headers[i])) {
-				while((line = br2.readLine()) != null) {
+				while((line = fileObjs.get("yAxisReader").readLine()) != null) {
 					String[] values = line.split(",");
 					colYData.add(values[i]);
 				}
 			}
 		}
+		
+		// Sort the array based on the data type
+		if (colXData.get(0).charAt(0)== '-' || colXData.get(0).charAt(0)== '0' || colXData.get(0).charAt(0)== '1' || colXData.get(0).charAt(0)== '2'
+				 || colXData.get(0).charAt(0)== '3' || colXData.get(0).charAt(0)== '4' || colXData.get(0).charAt(0)== '5' || colXData.get(0).charAt(0)== '6'
+				 || colXData.get(0).charAt(0)== '7' || colXData.get(0).charAt(0)== '8' || colXData.get(0).charAt(0)== '9') {
+			
+			for (int i=0; i < colXData.size(); i++) { 
+				tempList.add(Double.valueOf(colXData.get(i))); 
+	        }
 
-		for(int i=0; i < colXData.size(); i++) {
-			try {
-				Double colYval = Double.parseDouble(colYData.get(i));
-				series1.getData().add(new XYChart.Data(colXData.get(i), colYval));
-			}
-			catch (Exception e) {
-				System.out.println("Error converting the string to double");
-			}			
+			Collections.sort(tempList);
+			
+			for (int i=0; i < tempList.size(); i++) { 
+				newColXData.add(String.valueOf(tempList.get(i))); 
+	        }			
+		}
+				
+		// Scatterplot is for only numerical x and y axes
+		for(int i=0; i < newColXData.size(); i++) {
+			Double colYval = Double.parseDouble(colYData.get(i));
+			scatterChartData.getData().add(new XYChart.Data(newColXData.get(i), colYval));
 		}
 		
-        scatterChart.getData().add(series1);
+        scatterChart.getData().add(scatterChartData);
 	}
+ 	
+ 	@SuppressWarnings({ "unchecked", "rawtypes" })
+ 	public void showLineChart() {
+ 			
+ 			ArrayList<String> rowList = new ArrayList<>();
+	 		ArrayList<String> categoryList = new ArrayList<>();
+	 		ArrayList<Double> numericList = new ArrayList<>();
+	 		ArrayList<Integer> countList = new ArrayList<>();
+	 		
+	 		rowList.clear();
+	 		categoryList.clear();
+	 		numericList.clear();
+	 		countList.clear();
+	 		
+	 		lineChart.setAnimated(false);
+	 		lineChartData.getData().clear();
+	 		
+	 		final NumberAxis xAxisLine = new NumberAxis();
+	        final NumberAxis yAxisLine = new NumberAxis();
+	 		
+			try {
+				//opening the file
+				BufferedReader br = new BufferedReader(new FileReader(txt1Field.getText()));
+						
+				String line = br.readLine();
+				String[] headers = line.split(",");
+				
+										
+				//reading all the values
+				for (int i = 0; i < headers.length; i++) {
+					if (featureBox.getValue().equals(headers[i])) {
+						while((line = br.readLine()) != null) {
+							String[] values = line.split(",");
+							rowList.add(values[i]);
+						}
+					}
+				}
+				
+				
+				if (rowList.get(0).charAt(0)== '-' || rowList.get(0).charAt(0)== '0' || rowList.get(0).charAt(0)== '1' || rowList.get(0).charAt(0)== '2'
+						 || rowList.get(0).charAt(0)== '3' || rowList.get(0).charAt(0)== '4' || rowList.get(0).charAt(0)== '5' || rowList.get(0).charAt(0)== '6'
+						 || rowList.get(0).charAt(0)== '7' || rowList.get(0).charAt(0)== '8' || rowList.get(0).charAt(0)== '9') {
+					
+					numericList.add(Double.valueOf(rowList.get(0)));
+	 				
+	 				for (int m = 1; m < rowList.size(); m++) {
+	 					
+	 					int times = 0;
+	 					for (int b = 0; b < numericList.size(); b++) {
+	 						double s = Double.valueOf(rowList.get(m));
+	 						double v = numericList.get(b); 
+	 						
+	 						if (s != v) {
+	 							times++;		
+	 						}	
+	 					}
+	 					if (times == numericList.size()) {
+	 						numericList.add(Double.valueOf(rowList.get(m)));
+	 					}
+	 				}
+	 				
+	 				Collections.sort(numericList);
+	 				for (int s = 0; s < numericList.size(); s++) {
+	 					int count = 0;
+	 					for (int n = 0; n < rowList.size(); n++) {
+	 						double z = Double.valueOf(rowList.get(n));
+	 						double w = numericList.get(s);				
+	 						if (z == w) {
+	 							count++;
+	 							}	
+	 					}
+	 					countList.add(count);	
+	 				}
+				}else {
+					categoryList.add(rowList.get(0));
+	 				
+	 				for (int m = 1; m < rowList.size(); m++) {
+	 					
+	 					int times = 0;
+	 					for (int b = 0; b < categoryList.size(); b++) {
+	 											
+	 						if (!rowList.get(m).equals(categoryList.get(b))) {
+	 							times++;
+	 								
+	 						}
+	 						
+	 					}
+	 					
+	 					if (times == categoryList.size()) {
+	 						categoryList.add(rowList.get(m));
+	 						
+	 					}
+	 				}
+	 				
+	 				for (int s = 0; s < categoryList.size(); s++) {
+	 					int count = 0;
+	 					for (int n = 0; n < rowList.size(); n++) {
+	 						if (rowList.get(n).equals(categoryList.get(s))) {
+	 							count++;
+	 							}
+	 					}
+	 					countList.add(count);
+	 				}
+	 				
+	 				
+				}
+				
+				
+		
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	       
+			if (numericList.size() > categoryList.size()) {
+				for (int i = 0; i < numericList.size(); i++) {
+ 	 	    	   lineChartData.getData().addAll(new XYChart.Data(String.valueOf(numericList.get(i)), countList.get(i)));
+ 	 	       }
+ 	 	       
+ 	 	              
+ 	 	       
+ 	 	       if (lineChart != null) { 
+ 	 	    	   lineChart.getData().add(lineChartData);
+ 	 	    	   lineChart.setTitle(featureBox.getValue());
+ 	 	       }else {
+ 	 	    	   System.out.println("It is null!!!!!");
+ 	 	       }
+			}else {
+				for (int i = 0; i < categoryList.size(); i++) {
+ 	 	    	   lineChartData.getData().addAll(new XYChart.Data(categoryList.get(i), countList.get(i)));
+ 	 	       }
+ 	 	       
+ 	 	              
+ 	 	       
+ 	 	       if (lineChart != null) { 
+ 	 	    	   lineChart.getData().add(lineChartData);
+ 	 	    	   lineChart.setTitle(featureBox.getValue());
+ 	 	       }else {
+ 	 	    	   System.out.println("It is null!!!!!");
+ 	 	       }
+			}   
+		}
  	
  	public void buttonClear() {
  		barChartData.getData().clear();
@@ -357,6 +627,15 @@ public class MainController {
  		pieChart1.getData().removeAll(pieChartData);
  		pieChartData.removeAll(pieChartData);
  		pieChart1.setTitle("");
+ 		
+ 		scatterChartData.getData().clear();
+ 		scatterChart.getData().removeAll(scatterChartData);
+ 		scatterChart.setTitle("");
+ 		
+ 		lineChart.getData().clear();
+ 		lineChartData.getData().removeAll(scatterChartData);
+ 		lineChart.setTitle("");
+ 		
  	}
     
 
